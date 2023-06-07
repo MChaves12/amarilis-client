2849d5fa7e56387bf4abe29db204966c5fa1db0a
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-07 16:12:21 -0300

    api file and class created plus the methods of signuo, login and verify token

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      Amarílis
+      <Routes>
+        <Route path='/' element={<div>Amarílis</div>} />
+      </Routes>
     </div>
   );
 }
