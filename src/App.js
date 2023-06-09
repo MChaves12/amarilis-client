fa5ff830723b3559bdcd253a2ab4095038dacc22
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-09 15:06:13 -0300

    Signup page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom'
+import SignupPage from './pages/Signup';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<div>Amarílis</div>} />
+        <Route path='/admin/login' element={<SignupPage />} />
       </Routes>
     </div>
   );
