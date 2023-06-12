4fcc205c8608062636aa44c068f4e52e7645c474
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-12 10:07:44 -0300

    LoginPage created

---
+++ src/pages/Login/index.js
@@ -0,0 +1,50 @@
+import { useState, useContext } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
+import { AuthContext } from '../../context/auth.context';
+import api from '../../api/amarilis.api';
+
+function LoginPage() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    const { storeToken, authenticateUser } = useContext(AuthContext);
+
+    const navigate = useNavigate();
+
+    const handleUsername = (e) => setUsername(e.target.value);
+    const handlePassword = (e) => setPassword(e.target.value);
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const {authToken} = await api.login({username, password});
+        storeToken(authToken);
+        authenticateUser();
+        navigate('/account')
+      } catch (error) {
+        setErrorMessage(error.response.data.message);
+      }
+    }
+
+    return (
+        <div className='container'>
+            <h2>Login</h2>
+            <Form onSubmit={handleSubmit} className='form-container'>
+                 <Form.Group className="mb-3" controlId="formGroupEmail">
+                    <Form.Control className='form-input' type="text" placeholder="Username" onChange={handleUsername} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formGroupPassword">
+                    <Form.Control className='form-input' type="password" placeholder="Password" onChange={handlePassword} />
+                </Form.Group>
+                <button className='submit-btn' type='submit'>Sign up</button>
+            </Form>
+
+            {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+            <p>Don't have an account?</p>
+            <Link to="/signup">Signup</Link>
+        </div>
+    )
+}
+
+export default LoginPage;
