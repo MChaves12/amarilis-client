a1f018acd3ba32afaf743dc31d4ee7caceecd95d
Natalia Nossack <nvcordeiro22@gmail.com>
2023-06-12 21:01:44 +0200

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom'
-import SignupPage from './pages/Signup';
+import "./App.css";
+import "../src/components/Navbar/style.css"
+import { Routes, Route } from "react-router-dom";
+import SignupPage from "./pages/Signup";
+import MeuComponente from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <MeuComponente />
       <Routes>
-        <Route path='/' element={<div>Amarílis</div>} />
-        <Route path='/admin/signup' element={<SignupPage />} />
+        <Route path="/" element={<div></div>} />
+        <Route path="/admin/signup" element={<SignupPage />} />
       </Routes>
     </div>
   );
