812255bd1a65a3ce396ad2e083e58206f1159791
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-06-14 11:58:18 -0300

    styles to the ProductCrudPage made

--- src/pages/ProductCrud/index.js
+++ src/pages/ProductCrud/index.js
@@ -1,6 +1,7 @@
 import './styles.css';
 import { useState, useEffect, useCallback } from 'react';
 import { useParams, Link, useNavigate } from 'react-router-dom';
+import AdminNavbar from '../../components/AdminNavbar/AdminNavbar';
 import api from '../../api/amarilis.api';
 
 function ProductCrudPage () {
@@ -21,14 +22,17 @@ function ProductCrudPage () {
     const { productId } = useParams();
     const navigate = useNavigate();
 
-    const fechProduct = useCallback(async () => {
+    const fetchProduct = useCallback(async () => {
         const productFromDB = await api.getProduct(productId);
+        console.log(productFromDB);
         setProduct(productFromDB);
         setName(productFromDB.name);
         setPrice(productFromDB.price);
         setDescription(productFromDB.description);
         setSize(productFromDB.size);
     }, [productId]);
+
+    
 
     const deleteProduct = async () => {
         await api.removeProduct(productId);
@@ -43,49 +47,53 @@ function ProductCrudPage () {
         e.preventDefault();
         await api.editProduct({name, price, description, size});
         setIsFormOpen(false);
-        fechProduct();
+        fetchProduct();
     };
 
     useEffect(() =>{
-        fechProduct()
-    }, [fechProduct]);
+        fetchProduct()
+    }, [fetchProduct]);
 
     return(
-        <div className='product-crud'>
-            {product && (
-                <>
-                {isFormOpen ? (
-                    <form className='add-product-form-container' onSubmit={handleSubmit}>
-                        <label htmlFor="name">Nome:</label>
-                        <input className='input' id="name" type="text" value={name} onChange={handleName} />
-                    
-                        <label htmlFor="price">Preço:</label>
-                        <input className='input' id="price" type="text" value={price} onChange={handlePrice} />
-                    
-                        <label htmlFor="description">Descrição:</label>
-                        <textarea className='input' id="description" type='text' value={description} onChange={handleDescription} />
-                    
-                        <label htmlFor="size">Tamanho:</label>
-                        <input className='input' id="size" type="text" value={size} onChange={handleSize} />
-            
-                        <button type="submit">Adicionar produto</button>       
-                </form>
-                ) :(
+        <div>
+            <AdminNavbar />
+            <div className='product-crud'>
+                {product && (
                     <>
-                        <h2>{product.name}</h2>
-                        <span>{product.price}</span>
-                        <p>Tamanho: {product.size}</p>
-                        <p>{product.description}</p>
+                    {isFormOpen ? (
+                        <form className='add-product-form-container' onSubmit={handleSubmit}>
+                            <label htmlFor="name">Nome:</label>
+                            <input className='input' id="name" type="text" value={name} onChange={handleName} />
+                        
+                            <label htmlFor="price">Preço:</label>
+                            <input className='input' id="price" type="text" value={price} onChange={handlePrice} />
+                        
+                            <label htmlFor="description">Descrição:</label>
+                            <textarea className='input' id="description" type='text' value={description} onChange={handleDescription} />
+                        
+                            <label htmlFor="size">Tamanho:</label>
+                            <input className='input' id="size" type="text" value={size} onChange={handleSize} />
+                
+                            <button className='submit-form-btn' type="submit">Adicionar</button>       
+                    </form>
+                    ) :(
+                        <>
+                            <h2>{product.name}</h2>
+                            <span>{product.price}</span>
+                            <p>Tamanho: {product.size}</p>
+                            <p>{product.description}</p>
+                        </>
+                    )}
                     </>
                 )}
-                </>
-            )}
-
-            <button onClick={toggleIsFormOpen}>{isFormOpen ? 'Cancelar' : 'Editar'}</button>
-            <Link to='/products'>
-                 <button>Voltar</button>
-            </Link>
-            <button onClick={deleteProduct}>Remover</button>
+                <div className='btns-container'>
+                    <button className='form-btns' onClick={toggleIsFormOpen}>{isFormOpen ? 'Cancelar' : 'Editar'}</button>
+                    <Link to='/products'>
+                        <button className='form-btns'>Voltar</button>
+                    </Link>
+                    <button className='form-btns' onClick={deleteProduct}>Remover</button>
+                </div>        
+            </div>
         </div>
     );
 };
